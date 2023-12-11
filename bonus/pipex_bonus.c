aa77f70d65a7449b078ee00b3ec73f409be7a12c
Eduardo Castro Brito <educastro@MacBook-Air-de-Eduardo.local>
2023-12-11 17:37:53 -0300

    doc

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 14:39:26 by educastro         #+#    #+#             */
-/*   Updated: 2023/12/11 17:30:23 by educastro        ###   ########.fr       */
+/*   Created: 2023/11/08 16:16:07 by edcastro          #+#    #+#             */
+/*   Updated: 2023/12/11 17:37:42 by educastro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-int	main(int ac, char **av, char **envp)
+int	main(int ac, char **av)
 {
-	return (0);
+	
 }

73e46955717d5dbee0b5dcb52e89fabf04f11126
Eduardo Castro Brito <educastro@MacBook-Air-de-Eduardo.local>
2023-12-12 20:35:22 -0300

    add pipes redirectioners

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:16:07 by edcastro          #+#    #+#             */
-/*   Updated: 2023/12/12 20:20:58 by educastro        ###   ########.fr       */
+/*   Updated: 2023/12/12 20:35:15 by educastro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,33 @@ t_pipex	init(int ac, char **av)
 		is_error("output error", &d);
 	d.index = 0;
 	return (d);
+}
+
+void	dup_io(t_pipex *d)
+{
+	if (d->idx == 0)
+	{
+		dup2(d->fd[0], STDIN_FILENO);
+		dup2(d->pipe_b[WRITE], STDOUT_FILENO);
+	}
+	else if (d->idx == d->nb_cmds - 1)
+	{
+		dup2(d->fd[1], STDOUT_FILENO);
+		if (d->idx % 2 == 0)
+			dup2(d->pipe_b[READ], STDIN_FILENO);
+		else
+			dup2(d->pipe_a[READ], STDIN_FILENO); 
+	}
+	else if (d->idx % 2 == 0)
+	{
+		dup2(d->pipe_a[READ], STDIN_FILENO);
+    	dup2(d->pipe_b[WRITE], STDOUT_FILENO);
+	}
+	else if (d->idx % 2 != 0)
+	{
+		dup2(d->pipe_b[READ], STDIN_FILENO);
+		dup2(d->pipe_a[WRITE], STDOUT_FILENO);
+	}
 }
 
 int	main(int ac, char **av, char **envp)
